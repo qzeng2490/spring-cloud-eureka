abe70bd28ec29254a2d1358b0d59e8bbec463491
Qiang <qzeng2490@yahoo.com>
2019-03-21 16:02:18 +0800

    first commit

---
+++ consumer/src/main/java/qzeng2490/consumer/controller/ConsumerController.java
@@ -0,0 +1,20 @@
+package qzeng2490.consumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import qzeng2490.consumer.remote.HelloRemote;
+
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    qzeng2490.consumer.remote.HelloRemote HelloRemote;
+	
+    @RequestMapping("/hello/{name}")
+    public String index(@PathVariable("name") String name) {
+        return HelloRemote.hello(name);
+    }
+
+}
