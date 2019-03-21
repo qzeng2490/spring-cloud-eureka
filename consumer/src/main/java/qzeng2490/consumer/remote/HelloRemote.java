0f0c429871a7f5c3e9e646b03e42bfe8a0afba2e
Qiang <qzeng2490@yahoo.com>
2019-03-21 16:08:50 +0800

    熔断器Hystrix的使用

--- consumer/src/main/java/qzeng2490/consumer/remote/HelloRemote.java
+++ consumer/src/main/java/qzeng2490/consumer/remote/HelloRemote.java
@@ -7,11 +7,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 /**
  * Created by summer on 2017/5/11.
  */
-@FeignClient(name= "spring-cloud-producer")
+@FeignClient(name= "spring-cloud-producer", fallback = HelloRemoteHystrix.class)
 public interface HelloRemote {
 
     @RequestMapping(value = "/hello")
     public String hello(@RequestParam(value = "name") String name);
-
 
 }
